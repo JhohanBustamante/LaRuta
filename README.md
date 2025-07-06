6289ffbd574d2975b030d0198a8407ab1363ffa1
Jhohan B <jhohantma@gmail.com>
2025-07-06 00:30:48 -0500

    Se corrige error de estructuracion

--- README.md
+++
@@ -1,2 +0,0 @@
-# LaRuta
-Proyecto de SOFTWARE - ADSO. Sistema de información, entablado como punto de encuentro entre consumidor y asesor alrededor de la temática Fitness.
--- materialApoyo/2203/puente.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Content-Type: application/json'); //cabeceras para que el navegador sepa que es un archivo json
-
-    $json = file_get_contents('datos.json'); //lee el archivo json
-    
-    echo $json;                               //muestra el archivo json
-?>
